b2a019684cf31ea4558312f3c76b35fde7b49454
xavitouy <prof.javier.martin@gmail.com>
2025-04-01 11:28:04 -0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  site: 'https://jotaemeiuy.github.io',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  devToolbar: {
+    enabled: false
+  },
+  build: {
+    inlineStylesheets: "always",
+  },
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es"]
+  }
+});
