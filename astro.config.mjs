7b0d752a090adbf9b38389f002cddb1a3047f489
xavitouy <prof.javier.martin@gmail.com>
2025-04-01 12:18:50 -0300

    Modificacion static en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   site: 'https://jotaemeiuy.github.io',
   vite: {
     plugins: [tailwindcss()],
