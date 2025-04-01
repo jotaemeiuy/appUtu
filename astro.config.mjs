b74902c1a7d7602bf6ee491e3b6c74dcf6eb94f1
xavitouy <prof.javier.martin@gmail.com>
2025-04-01 13:50:59 -0300

    Modificacion de links para produccion y local

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jotaemeiuy.github.io',
-  base: 'appUtu',
+  base: '/',
   output: "static",
   adapter: vercel(),
   vite: {
