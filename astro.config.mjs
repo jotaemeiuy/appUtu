ac27a17a18e0b49b56296a0d5f4b18b7eaf4dd38
xavitouy <prof.javier.martin@gmail.com>
2025-04-01 14:05:20 -0300

    Modificacion Header.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jotaemeiuy.github.io',
-  base: 'appUtu',
+  base: '/appUtu',
   output: "static",
   adapter: vercel(),
   vite: {
